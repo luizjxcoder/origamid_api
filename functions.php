a6ccbe47ab236c37d23efad7687f6acafaba31da
jxcoder <luiz.jun19@gmail.com>
2022-06-28 16:26:52 -0300

    api feita no curso de rest api Origamid

---
+++ functions.php
@@ -0,0 +1,30 @@
+<?php
+// remove_action('rest_api_init', 'create_initial_rest_routes', 99);
+
+$dirbase = get_template_directory();
+
+require_once $dirbase . '/endpoints/user_post.php';
+require_once $dirbase . '/endpoints/user_get.php';
+
+require_once $dirbase . '/endpoints/photo_post.php';
+require_once $dirbase . '/endpoints/photo_get.php';
+require_once $dirbase . '/endpoints/photo_delete.php';
+
+require_once $dirbase . '/endpoints/comment_post.php';
+require_once $dirbase . '/endpoints/comment_get.php';
+
+require_once $dirbase . '/endpoints/stats_get.php';
+
+require_once $dirbase . '/endpoints/password.php';
+
+function change_api() {
+  return 'json';
+}
+add_filter('rest_url_prefix', 'change_api');
+
+function expire_token() {
+  return time() + (60 * 60 * 24);
+}
+add_action('jwt_auth_expire', 'expire_token');
+
+?>
